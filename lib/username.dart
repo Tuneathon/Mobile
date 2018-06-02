50884018d50179136561e283802eed1ccc46c62b
Borislav Statev <bstatev07@gmail.com>
2018-06-02 15:44:23 +0300

    Create user call to the service

--- lib/username.dart
+++ lib/username.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 final String usernameKey = "username";
+final String userIdKey = "userId";
 
 void saveUsername(BuildContext context, String username) async {
-  Navigator.pop(context);
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setString(usernameKey, username);
+  Map<String, String> userMap = {"name": username};
+  String userData = json.encode(userMap); 
+  Map<String, String> lHeaders = {"Content-type": "application/json", 
+    "Accept": "application/json"};
+  final response = await http.post(
+    'http://10.15.16.240:8080/user/create', body: userData, headers: lHeaders);
+  if (response.statusCode == 200){
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString(usernameKey, username);
+      await prefs.setInt(userIdKey, int.parse(response.body));
+      Navigator.pop(context);
+  }
 }
 
 void showUsernameDialog(BuildContext context) async
 {
     String username;
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    //Bobbyprefs.remove(usernameKey);
+    //prefs.remove(usernameKey);
     if (prefs.getString(usernameKey) == null){
       showDialog(
         context: context,
