e24b9440a3294698c9b0ecb307dba13da1a22e82
Firas Al-Houssari <vladimir.sopotlyanov@accedia.com>
2018-06-02 10:46:33 +0300

    Create template game room page

---
+++ lib/game_room.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GameRoomPage extends StatefulWidget{
+  @override
+  _GameRoomPageState createState() => new _GameRoomPageState();
+}
+
+class _GameRoomPageState extends State<GameRoomPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("GameRoom"),
+        ),
+      body: new Container(
+
+      )
+    );
+  }
+}
